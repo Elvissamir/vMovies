10804f698811842cb89f01b0e06f5484d6412202
Elvis <elvissamir18@gmail.com>
2022-02-09 05:47:24 -0400

    Added code for mongodb connection using mongoose.

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const helmet = require('helmet')
 const mongoose = require('mongoose')
 const Joi = require('joi')
 
+// MONGO CONNECTION
+async function connectToDB() {
+    const mongoBaseUrl = (process.env.DEV_USING_DOCKER == "true")? process.env.DEV_MONGO_CONTAINER_URL : process.env.DEV_MONGO_LOCAL_URL 
+    const databaseName = process.env.DATABASENAME
+    const connectionUrl = `${mongoBaseUrl}/${databaseName}`
+
+    console.log('(MONGOOSE) Using url:',connectionUrl)
+    await mongoose.connect(mongoBaseUrl)
+}
+
+connectToDB().then(() => console.log('(MONGOOSE) Connected to MONGODB...'))
+           .catch((e) => console.log('(MONGOOSE) Could not connect to database...', e))
+
 // MIDDLEWARE
 app.use(helmet())
 
